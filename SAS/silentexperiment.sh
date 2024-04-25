98d06cea52df07e13d9c23ba905a6b5ef79449a3
Kaveh Sari <ksari6000@yahoo.com>
2024-04-25 13:50:47 -0400

    created two files

--- SAS/silentexperiment.sh
+++ SAS/silentexperiment.sh
@@ -1,2 +1,2 @@
 sqlplus -s eric/ericdata@draix22.usmc-mccs.org/sastst @SilentExperiment.sql > /tmp/today
-sqlplus  eric/ericdata@draix22.usmc-mccs.org/sastst @SilentExperiment.sql >> /tmp/today
+sqlplus  eric/ericdata@draix22.usmc-mccs.org/sastst @SilentExperiment.sql > /tmp/today2
