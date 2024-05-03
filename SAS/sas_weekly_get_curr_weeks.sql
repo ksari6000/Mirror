77b617b63ddf1934c0d3e54bcf5c2f867df0d2fd
Kaveh Sari <ksari6000@yahoo.com>
2024-05-03 14:02:33 -0400

    Moved SAS files from PS/SAS to Mirror/SAS

---
+++ SAS/sas_weekly_get_curr_weeks.sql
@@ -0,0 +1,12 @@
+
+set head off
+set linesize 1000
+set colsep ,
+set pagesize 0
+set tab off
+set wrap off
+Select 'rec',Mc.Merchandising_Year, Mc.Merchandising_Week, Mc.Merchandising_Period
+From (Select * From Merchandising_Calendars Where Business_Unit_Id = 30 And Week_Ending_Date <= Trunc(Sysdate) 
+Order By Merchandising_Year Desc, Merchandising_Week Desc) Mc
+          Where Rownum <= 1;
+exit
---
+++ SAS/sas_weekly_prev_start.sql
@@ -0,0 +1,8 @@
+set head off
+set linesize 1000
+set colsep ,
+set pagesize 0
+set tab off
+set wrap off
+exec p_wkly_time_period(5);
+exit
